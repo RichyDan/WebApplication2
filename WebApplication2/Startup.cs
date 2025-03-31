7a50b4d46e3afccf8b81a227eb19e5770be49d7a
Daniil <rychenkov16@yandex.ru>
2025-03-31 21:49:50 +0300

    Add project files.

---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,78 @@
+//using Asp.NET.MiddleWares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        static IWebHostEnvironment _env;
+
+
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"{_env.ApplicationName} - ASP.Net Core tutorial project");
+            });
+        }
+
+        private static void Config(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync($"App name: {_env.ApplicationName}. App running configuration: {_env.EnvironmentName}");
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            Console.WriteLine($"Launching project from: {env.ContentRootPath}");
+            _env = env;
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            //app.UseMiddleware<LoggingMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
+                });
+            });
+
+            app.Map("/about", About);
+            app.Map("/config", Config);
+
+            app.UseStatusCodePages();
+
+            //app.Run(async (context) =>
+            //{                
+            //    await context.Response.WriteAsync($"Page not found");
+            //});
+
+        }
+
+
+    }
+
+
+}
